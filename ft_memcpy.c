ed87850fc94afb50b6dc0422ac1f65417e675305
kinou-p <alexpomms@gmail.com>
2020-12-13 21:11:16 +0100

    push

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: apommier <apommier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 00:12:03 by apommier          #+#    #+#             */
-/*   Updated: 2020/12/13 21:03:38 by apommier         ###   ########.fr       */
+/*   Updated: 2020/12/13 21:09:28 by apommier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char		*p;
 	const char	*p1;
 
+	if (!dest || !src || !n)
+		return (dest);
 	p = (char*)dest;
 	p1 = (const char*)src;
 
