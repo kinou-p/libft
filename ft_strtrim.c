c8b63ea430f64134983d06fec7df8ba8bf65c20d
kinou-p <alexpomms@gmail.com>
2020-12-16 16:22:11 +0100

    push

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: apommier <apommier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 23:52:05 by apommier          #+#    #+#             */
-/*   Updated: 2020/12/16 16:15:30 by apommier         ###   ########.fr       */
+/*   Updated: 2020/12/16 16:21:56 by apommier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,11 @@ char		*ft_strtrim(char const *s1, char const *set)
 	if (dest == 0 || s1 == 0)
 		return (0);
 	j = 0;
-	while (s1[i++] && len - j++ && len > 0)
+	while (s1[i] && len - j && len > 0)
+	{
 		dest[j] = s1[i];
-	dest[j] = 0;
+		i++;
+		j++;
+	}
 	return (dest);
 }
